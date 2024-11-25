d0052eab5982757335684e595097dae04805c17d
Vladyslav <vlazav01@easv365.dk>
2024-11-25 18:59:16 +0100

    newRepo
    has been added something

---
+++ src/main/java/dk/easv/mytunes/DAL/db/MusicDAO_DB.java
@@ -0,0 +1,6 @@
+package dk.easv.mytunes.DAL.db;
+
+import dk.easv.mytunes.DAL.IMusicDataAcces;
+
+public class MusicDAO_DB implements IMusicDataAcces {
+}
---
+++ src/main/java/dk/easv/mytunes/GUI/MainController.java
@@ -0,0 +1,5 @@
+package dk.easv.mytunes.GUI;
+
+public class MainController {
+
+}
