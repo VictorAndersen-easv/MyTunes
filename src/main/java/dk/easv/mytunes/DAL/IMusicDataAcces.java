46a106b9ab4a3bcc70e0732346ac4ca96d9dc85c
Vladyslav <vlazav01@easv365.dk>
2024-11-27 11:13:38 +0100

    trying make it work

--- src/main/java/dk/easv/mytunes/DAL/IMusicDataAcces.java
+++ src/main/java/dk/easv/mytunes/DAL/IMusicDataAcces.java
@@ -1,4 +1,9 @@
 package dk.easv.mytunes.DAL;
 
+import dk.easv.mytunes.BE.Song;
+
+import java.util.List;
+
 public interface IMusicDataAcces {
+    List<Song> getAllMusic() throws Exception;
 }
--- src/main/java/dk/easv/mytunes/GUI/MainController.java
+++ src/main/java/dk/easv/mytunes/GUI/MainController.java
@@ -1,5 +1,40 @@
 package dk.easv.mytunes.GUI;
 
-public class MainController {
+import dk.easv.mytunes.DAL.db.DBConnector;
+import dk.easv.mytunes.GUI.Model.MusicModel;
+import javafx.fxml.Initializable;
+
+import java.awt.event.ActionListener;
+import java.net.URL;
+import java.sql.ResultSet;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable{
+
+    private MusicModel musicModel;
+
+
+
+
+    public MainController()  {
+
+        try {
+            musicModel = new MusicModel();
+        } catch (Exception e) {
+            displayError(e);
+            e.printStackTrace();
+        }
+    }
+
+    private void displayError(Exception e) {
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
 
 }
