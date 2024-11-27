dd9010f3d36c8ba6d0618cc9917dc1b20ecf1b46
MiaSofie01 <miangaard@outlook.dk>
2024-11-27 10:43:24 +0100

    defineret knapperne i intellij

--- src/main/java/dk/easv/mytunes/GUI/MainController.java
+++ src/main/java/dk/easv/mytunes/GUI/MainController.java
@@ -1,5 +1,26 @@
 package dk.easv.mytunes.GUI;
 
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
 public class MainController {
 
+    @FXML
+    private Button playbtn;
+
+    @FXML
+    private Button pausebtn;
+
+    @FXML
+    private Button previousbtn;
+
+    @FXML
+    private Button searchbtn;
+
+    @FXML
+    private Button nextbtn;
+
+
 }
+
